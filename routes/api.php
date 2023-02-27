2dcf99eb1541699fee1affb47ab8c850b36b98f1
mitcool <dimitaraleksiev4@gmail.com>
2023-02-27 08:47:37 +0200

    Add zones get method

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\Api\V1\Auth;
 use \App\Http\Controllers\Api\V1\VehicleController;
+use \App\Http\Controllers\Api\V1\ZoneController;
  
  
 Route::post('auth/register', Auth\RegisterController::class);
-
 Route::post('auth/login', Auth\LoginController::class);
-
+Route::get('zones',[ZoneController::class,'index']);
 
 Route::middleware('auth:sanctum')->group(function (){
     Route::get('profile', [Auth\ProfileController::class, 'show']);
